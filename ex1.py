ce77cfaed8fda895eaa1d53a39ff614f757713e7
Mike Sims <mikesims321@gmail.com>
2014-09-09 17:39:58 -0700

    First commit

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print "Hello World!"
+print "Hello Again!"
+print "I likr typing this!"
+print "This is fun!" 
+print 'Yay! Printing.'
+print "I'd much rather you not"
+print 'I "said" do  ot touch this.'
